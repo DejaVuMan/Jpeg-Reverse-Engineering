a145b227b271309d0a92775b584001508dd3ec96
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-05-05 22:22:20 +0200

    Added initialization for quantization tables and huffman tables inside of main, began creating switch cases for decoding

--- src/JpegHelpers/JpegDecoder.java
+++ src/JpegHelpers/JpegDecoder.java
@@ -3,10 +3,9 @@ package JpegHelpers;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class JpegDecoder { // JPG and JPEG are the same thing :)
+class JpegDecoder { // JPG and JPEG are the same thing :)
 
     /*
     0xFFD8 - Start of Image
@@ -19,6 +18,20 @@ public class JpegDecoder { // JPG and JPEG are the same thing :)
     0xFFDA - Start Scan
     0xFFD9 - End of Image
      */
+
+    private String fileName;
+    private Map<Integer, HuffmanTable> huffmanTables; // <ht header, ht> DC Y, CbCr : 0, 1 AC Y, CbCr : 16, 17
+    private Map<Integer, int[]> quantizationTables;
+
+    private int precision;
+    private int height;
+    private int width;
+    private int mcuWidth;
+    private int mcuHeight;
+    private int mcuHSF; // Horizontal Sample Factor
+    private int mcuVSF; // Vertical Sample Factor
+    private boolean color; // chroma components in JPEG
+    private int mode; // 0 for Baseline
 
     public void decode(String image) throws IOException {
         int[] jpegImageData;
@@ -33,11 +46,34 @@ public class JpegDecoder { // JPG and JPEG are the same thing :)
         }
 
         // Init Values
-        //TODO: Create Hashmaps for quant and huff, filenames, modemarkers
+        quantizationTables = new HashMap<>();
+        huffmanTables = new HashMap<>();
+        fileName = image.substring(0, image.lastIndexOf('.'));
+        mode = -1; // Uninitialized Mode
+
+        System.out.println("Reading Jpeg File: " + fileName);
 
         // Decoding
-        //TODO: Implement cases for various bit data
-
+        main: for (int i = 0; i < jpegImageData.length; i++) {
+            if(jpegImageData[i] == 0xFF){
+                int marker = jpegImageData[i] << 8 | jpegImageData[i + 1];
+                switch(marker) {
+                    case 0xFFE0 -> System.out.println("This file is in JFIF format");
+                    case 0xFFE1 -> System.out.println("This file is in EXIF format");
+                    case 0xFFC4 -> {
+                        int length = jpegImageData[i + 2] << 8 | jpegImageData[i + 3];
+                        decodeHuffmanTables(Arrays.copyOfRange(jpegImageData, i + 4, i + 2 + length));
+                    }
+                    // more cases here
+                }
+            }
+        }
     }
+
+    private void decodeHuffmanTables(int[] chunk){
+        int cd = chunk[0];
+        // ...
+    }
+
 
 }
