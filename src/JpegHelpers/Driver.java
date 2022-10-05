e658f63dcefb951a30dd825003a60296d5eddd53
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-10-05 18:51:45 +0200

    Imported java.io.File, javax.imageio.ImageIO for sake of time saving with JPEG compression

--- src/JpegHelpers/Driver.java
+++ src/JpegHelpers/Driver.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Driver {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        if(args.length != 1){
 //            System.out.println("Usage: java jpegdecode <filename>");
 //            return;
@@ -18,7 +18,7 @@ public class Driver {
 //            return;
 //        }
 
-        String filePath = "asianchildmoment.jpg";
+        String filePath = "asianchildmoment.bmp"; // "asianchildmoment.jpg";
         // 2048x1536 -> 256 x 192 blocks -> Color Transform -> Apply DCT ->
         // Quantization (tables) -> Serialization (zig-zag)-> Vectoring(dpcm) -> Encoding (huffman)
         //Small_pict_test.JPG returns SMALLER resolution?
@@ -30,12 +30,22 @@ public class Driver {
 
         // might be something to do with encoding process of the JPGs, this is a "basic" decoder
 
-        try{
-            new JpegDecoder().decode(filePath); // args[0]
-        } catch(FileNotFoundException e){
+//        try {
+//            new JpegDecoder().decode(filePath); // args[0]
+//        } catch (FileNotFoundException e) {
+//            System.err.println("File not found! Make sure you're using a valid file.");
+//        } catch (IOException e) {
+//            System.err.println("An IO Exception occurred: " + e.getLocalizedMessage());
+//        }
+//    }
+//}
+
+        try {
+            new JpegEncoder().Encode(filePath);
+        } catch(FileNotFoundException e) {
             System.err.println("File not found! Make sure you're using a valid file.");
-        } catch(IOException e){
+        } catch(IOException e) {
             System.err.println("An IO Exception occurred: " + e.getLocalizedMessage());
         }
-    }
+        }
 }
--- src/JpegHelpers/JpegEncoder.java
+++ src/JpegHelpers/JpegEncoder.java
@@ -1,24 +1,23 @@
 package JpegHelpers;
 
+import java.awt.image.BufferedImage;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.imageio.ImageIO;
 
 public class JpegEncoder {
 
     void Encode(String image) throws IOException{
-        int[] rawImageData;
-        try(DataInputStream dataInputStream = new DataInputStream(new FileInputStream(image))){
-            List<Integer> dataList = new ArrayList<>();
-
-            while(dataInputStream.available() > 0)
-            {
-                int uByte = dataInputStream.readUnsignedByte();
-                dataList.add(uByte);
-            }
-            rawImageData = dataList.stream().mapToInt(Integer::intValue).toArray();
-        }
+        BufferedImage imageBuff = ImageIO.read(new File(image));
+        System.out.println(imageBuff.getWidth());
+        System.out.println(imageBuff.getHeight());
     }
+
+//    Integer[] RgbToYcbcr(Integer[] rawData)
+//    {
+//    }
 }
