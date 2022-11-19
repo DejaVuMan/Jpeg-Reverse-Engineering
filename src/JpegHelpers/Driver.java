f1d27820e9768e5b9f44cf7dcaedb192f207462e
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-11-19 14:07:51 +0100

    Implemented rudimentary GUI for encoding of BMP to JPEG

--- src/JpegHelpers/Driver.java
+++ src/JpegHelpers/Driver.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.time.Instant;
 
 public class Driver {
+    private static String path = ""; // empty initial path to be changed on button press.
     public static void main(String[] args) {
 //        String filePath = "bmp_1080.bmp";
 //        String filePath2 = "bmp_400_jwork.jpg";
@@ -22,10 +23,64 @@ public class Driver {
 //        } catch(IOException e) {
 //            System.err.println("An IO Exception occurred: " + e.getLocalizedMessage());
 //        }
+    JFrame mainWindow = new JFrame("JPEG Encoder");
+
+    JButton encodeButton = new JButton("Encode (BMP to JPEG)");
+    JTextField filePathField = new JTextField();
+    JButton setPath = new JButton("Set Path");
+    JButton decodeButton = new JButton("Decode (JPEG to BMP)");
+    JLabel pathLabel = new JLabel("File Path:");
+
+    encodeButton.setBounds(40,90,200,30);
+    filePathField.setBounds(100, 50, 140, 30);
+    pathLabel.setBounds(40, 50, 100, 30);
+    setPath.setBounds(250, 50, 100, 30);
+    decodeButton.setBounds(40, 130, 200, 30);
 
 
+    mainWindow.add(encodeButton);
+    mainWindow.add(filePathField);
+    mainWindow.add(setPath);
+    mainWindow.add(decodeButton);
+    mainWindow.add(pathLabel);
 
+    mainWindow.setSize(400,400);
+    mainWindow.setLayout(null);
+    mainWindow.setVisible(true);
+
+    setPath.addActionListener(e -> {
+        path = filePathField.getText();
+        System.out.println("Path set to: " + path);
+    });
+
+    encodeButton.addActionListener(e -> {
+        if(path.equals("")){
+            System.out.println("Please set a path first!");
+            return;
+        } else if (!path.endsWith(".bmp")){
+            System.out.println("Please set a valid BMP file path!");
+            return;
+        }
+        try {
+            Instant start = Instant.now();
+            new JpegEncoder().Encode(path); // this is jpeg encoder
+            Instant end = Instant.now();
+            System.out.println("Total Time elapsed: " + java.time.Duration.between(start, end).toMillis() + "ms");
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        JFrame doneWindow = new JFrame("Encoding Done");
+        JLabel doneLabel = new JLabel("Encoding Done!");
+        doneLabel.setBounds(40, 50, 100, 30);
+        doneWindow.add(doneLabel);
+        doneWindow.setSize(200, 200);
+        doneWindow.setLayout(null);
+        doneWindow.setVisible(true);
+    });
     }
+
 
     // 2048x1536 -> 256 x 192 blocks -> Color Transform -> Apply DCT ->
     // Quantization (tables) -> Serialization (zig-zag)-> Vectoring(dpcm) -> Encoding (huffman)
